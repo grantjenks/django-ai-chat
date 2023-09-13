61f2cadcb2ca36555c1f71f2de62c2fac13310ca
Grant Jenks <grant.jenks@gmail.com>
2023-09-12 22:05:39 -0700

    Move api key to env var

--- tests/www/settings.py
+++ tests/www/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,4 +126,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-DJANGO_AI_CHAT_OPENAI_API_KEY = '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'
+DJANGO_AI_CHAT_OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', 'NOT SET')
